b50b354ead49ac4d1343753f962cbfaff19131fc
D. Vespa <d.vespa@gmail.com>
2013-05-19 18:15:12 -0300

    criação de sistema de sprite que detecta posição do personagem e descobre se qual set de movimentos tem que disparar

--- lombrinha/Assets/animaNPC.js
+++ lombrinha/Assets/animaNPC.js
@@ -1,27 +1,71 @@
 var positions;
-var startFrame = 3;
-var totalFrames = 10;
-var startLine = 0;
-var spriteDistance:float;
+var startFrame:float = 3;
+var totalFramesPerLine:float = 10;
+var spriteLines:float = 2;
+var startLine = 2;
+var numberlines;
+var framesPerSecond = 24;
+
+var currentLeft;
+var currentUp;
 
 function setFirstPos(){
-	var pos:float = startFrame/totalFrames;
-	renderer.material.mainTextureOffset = Vector2 (pos, startLine);
+	var pos:float = startFrame/totalFramesPerLine;
+	numberlines  = 1.0/spriteLines;
+	var line = numberlines * startLine;
+	renderer.material.mainTextureOffset = Vector2 (pos, line);
 }
 
 function Start () {
+
+	 currentLeft = transform.position.x;
+	 currentUp	= transform.position.y;
+	 
 	 setFirstPos();
 	 positions = {
-		"right":[1,[7,8,7,9]],
-		"left":	[2,[6,7,8]],
-		"up":	[1, [0,1,2]],
-		"down":	[1, [3,4,5]]
+		"right":[1,[4,5,6]],
+		"left":	[1,[7,8,9,8]],
+		"up":	[2, [0,1,2]],
+		"down":	[2, [3,4,5]]
 	};
 }
 
+//identify the atual position and change to the next
+function setNewPostion(t, type){
+	var index : int = Time.time * framesPerSecond;
+	//
+	var pos = positions[type];
+	var line = numberlines * pos[0];
+	var ct = pos[1].length;
+	index = index % (ct);
+	var currentPos = (1.0/totalFramesPerLine) * pos[1][index];
+	t.renderer.material.mainTextureOffset = Vector2 (currentPos, line);
+}
+	
 function moveNPC(){
-
-
+	var tolerate = 0.4;
+	var newPosX = transform.position.x;
+	var newPosY = transform.position.y;
+	
+	var difX = Mathf.Abs(newPosX- currentLeft);
+	var difY = Mathf.Abs(newPosY - currentUp);
+	Debug.Log(difX + "," +difY);
+	//
+	if(difY > difX){
+		if (currentUp < newPosY){
+			setNewPostion(this, "up");
+		}else{
+			setNewPostion(this, "down");
+		}
+	}else{
+		if((currentLeft) < newPosX  ){
+			setNewPostion(this, "left");
+		}else if(currentLeft > newPosX){
+			setNewPostion(this, "right");
+		}
+	};
+	currentUp = newPosY;
+	currentLeft = newPosX;
 }
 
 function Update () {
