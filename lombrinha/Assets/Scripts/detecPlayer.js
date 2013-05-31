89659254f1ebf2da129cdfa5866db71fe1691071
Jakeline Santos <jakeline.santos@gmail.com>
2013-05-31 20:55:11 -0300

    revisao perseguicao

--- lombrinha/Assets/Scripts/detecPlayer.js
+++ lombrinha/Assets/Scripts/detecPlayer.js
@@ -1,17 +1,26 @@
 #pragma strict
 
+var target : Transform;
+var initialSafeDistance : int = 3;
+var safeDistance : int = initialSafeDistance;
+var distanceToPlayer : float;
 
-var target:Transform;
-var chao: GameObject;
 function Start () {
-
+	target = GameObject.Find("player").transform;
 }
 
 function Update () {
-	var distanceToPlayer = Vector3.Distance(this.transform.position, target.transform.position);
-	var x = target.transform.position.x -this.transform.position.x ;
-	var y = target.transform.position.y -this.transform.position.y ;
-	var z = target.transform.position.z - this.transform.position.z;
-	transform.Translate(Vector3(x,y,z) * (Time.deltaTime/distanceToPlayer)*9);
+	distanceToPlayer = Vector3.Distance(this.transform.position, target.transform.position);
 	
+	if (distanceToPlayer < safeDistance) {
+		var x = target.transform.position.x - this.transform.position.x;
+		var y = target.transform.position.y - this.transform.position.y;
+		var z = target.transform.position.z - this.transform.position.z;
+		transform.Translate(Vector3(x,y,z) * (Time.deltaTime/distanceToPlayer)*9);
+		safeDistance = initialSafeDistance * 5;
+		this.GetComponent(detectaCaminho).lastPositions = new Array();
+	}
+	else {
+		safeDistance = initialSafeDistance;
+	}
 }
