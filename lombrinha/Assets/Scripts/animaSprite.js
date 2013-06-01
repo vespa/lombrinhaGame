14b018e3c317a0604088437b6ca94f7552a59969
D. Vespa <d.vespa@gmail.com>
2013-06-01 18:11:20 -0300

    merge dos arquivos de ontem. Finalização do comportamento do balao e dos vendedores

--- lombrinha/Assets/Scripts/animaSprite.js
+++ lombrinha/Assets/Scripts/animaSprite.js
@@ -3,6 +3,7 @@ var totalFramesPerLine:float = 10;
 var startLine = 2;
 var spriteLines:float = 2;
 var numberlines;
+var currentDirection;
 // speed of sprite changing. The zero in the end IS important.
 var framesPerSecond:float =20.0;
 // these two just starts vars where we'll calculate where are the frames positions
@@ -16,7 +17,9 @@ var positions = {
 	"right":[2,[0,1,2]],
 	"left":	[1,[6,7,8]],
 	"up":	[1, [0,1,2]],
-	"down":	[1, [3,4,5]]
+	"down":	[1, [3,4,5]],
+	"atackLeft": [2, [3,4,5]],
+	"atackRight": [2, [6,7,8]]
 };
 
 function getSpriteDistance(){
@@ -31,6 +34,7 @@ function getSpriteDistance(){
 function setNewPostion(t, type){
 	var index : int = Time.time * framesPerSecond;
 	//
+	currentDirection = type;
 	var pos = positions[type];
 	var line = numberlines * pos[0];
 	var ct = pos[1].length;
@@ -58,17 +62,29 @@ function move(){
 	if(Input.GetKey(KeyCode.LeftArrow)){
 		setNewPostion(this, "left"); 
 	}	
+	//
+	if (Input.GetKey(KeyCode.Space)){
+		if (currentDirection == "left" || currentDirection == "atackLeft"){
+			setNewPostion(this, "atackLeft"); 
+		}else{
+			setNewPostion(this, "atackRight"); 
+		}
+		
+	}
 }
 
 function Start () {
 	textureSize 	= renderer.material.mainTexture.width;
 	spriteDistance 	= getSpriteDistance();
 	numberlines  = 1.0/spriteLines;
-	
 	player = GameObject.Find("player");
 }
 
 function Update () {
-	if (player.GetComponent(animaPlayer).parado == false)
+	if (player.GetComponent(animaPlayer).parado == false || Input.GetKey(KeyCode.Space)){
 		move();
+	}else{
+		renderer.material.mainTextureOffset = Vector2 (0.3, 0.5);
+	}
+		
 }
--- lombrinha/Assets/Scripts/vendedorAnimate.js
+++ lombrinha/Assets/Scripts/vendedorAnimate.js
@@ -2,9 +2,10 @@ var totalFramesPerLine:float = 5;
 var spaceBetweenFrames:float = 1.0/totalFramesPerLine;
 var framesPerSecond:int = 3;
 var isAtacked:boolean = false;
+var finishAtack:boolean = false;
 var lookingPos:Array = [0, 1, 0, 2];
 var atackedPos:Array = [3,4];
-var finalCountDown = atackedPos.length;
+var Timer = 0.0;
 
 function looking(t){
 	var index : int = Time.time * framesPerSecond;
@@ -15,23 +16,23 @@ function looking(t){
 	t.renderer.material.mainTextureOffset = Vector2 (currentPos, 0);
 }
 
-function dead(t){
-	if(finalCountDown != 0){
-		var index = Time.time*5;
-		var pos = atackedPos;
-		var ct = pos.length;
-		index = index % (ct);
-		var currentPos = (1.0/totalFramesPerLine) * pos[index];
-		t.renderer.material.mainTextureOffset = Vector2 (currentPos, 0);	
-		finalCountDown--;
-	}else{
-	  return false;
+function dead(t){	
+	var index : int = Timer *3;
+	index = Mathf.Floor(index);
+	var pos = atackedPos;
+	var ct = pos.length;
+	index = index % (ct);
+	var currentPos = (1.0/totalFramesPerLine) * pos[index];
+	t.renderer.material.mainTextureOffset = Vector2 (currentPos, 0);	
+	if(index == (ct-1)){
+		finishAtack = true;
 	}
+	Timer += Time.deltaTime;	
 }
 function Update () {
 	if(!isAtacked){
 		looking(this);
-	}else{
+	}else if (!finishAtack){
 		dead(this);
 	}
 }
